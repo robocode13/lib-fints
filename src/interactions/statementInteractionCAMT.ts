4ee7274b676abbe3a8f1e4861afc1ce8c9be09ae
Anton Schegg <aschegg@opentext.com>
2026-01-22 17:05:36 +0100

    If xml encoding is utf-8, convert it accordingly

--- src/interactions/statementInteractionCAMT.ts
+++ src/interactions/statementInteractionCAMT.ts
@@ -53,12 +53,20 @@ export class StatementInteractionCAMT extends CustomerOrderInteraction {
 				// Parse all CAMT messages (one per booking day) and combine statements
 				const allStatements: Statement[] = [];
 				for (const camtMessage of hicaz.bookedTransactions) {
-					// camtMessage is initially encoded as 'latin1' (ISO-8859-1), but actually contains UTF-8 data.
-					// Therefore, we need to first convert it back to a buffer using 'latin1', and then decode it as 'utf8'.
-					const intermediateBuffer = Buffer.from(camtMessage, 'latin1');
-					const utf8String = intermediateBuffer.toString('utf8');
+					// The regex looks for the XML declaration `<?xml ... ?>`
+					// and checks if it contains the attribute encoding="UTF-8".
+					// The 'i' flag makes the match case-insensitive (e.g., for "utf-8").
+					const isUtf8Encoded = /<\?xml[^>]*encoding="UTF-8"[^>]*\?>/i.test(camtMessage);
 
-					const parser = new CamtParser(utf8String);
+					let xmlString: string = camtMessage;
+					if (isUtf8Encoded) {
+						// camtMessage is initially encoded as 'latin1' (ISO-8859-1), but actually contains UTF-8 data.
+						// Therefore, we need to first convert it back to a buffer using 'latin1', and then decode it as 'utf8'.
+						const intermediateBuffer = Buffer.from(camtMessage, 'latin1');
+						xmlString = intermediateBuffer.toString('utf8');
+					}
+
+					const parser = new CamtParser(xmlString);
 					const statements = parser.parse();
 					allStatements.push(...statements);
 				}
